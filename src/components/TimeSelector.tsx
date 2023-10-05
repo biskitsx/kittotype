0144b8e050f6c23b7ff96c699e0a0cb58894af7c
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-10-05 10:42:25 +0700

    add time picker

---
+++ src/components/TimeSelector.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect } from 'react'
+
+function TimeSelector({ typing, setTimeUp, setTyping }: { typing: string, setTimeUp: React.Dispatch<React.SetStateAction<boolean>>, setTyping: React.Dispatch<React.SetStateAction<string>> }) {
+    const [time, setTime] = React.useState(15)
+    const [btnActive, setBtnActive] = React.useState(15)
+    const [startCountdown, setStartCountdown] = React.useState(false)
+    const onBtnClick = (e: any) => {
+        setTyping('')
+        setStartCountdown(false)
+        setTime(e.target.value)
+        setBtnActive(e.target.value)
+    }
+
+    useEffect(() => {
+        if (typing.length == 1) {
+            setStartCountdown(true)
+        }
+    }, [typing])
+
+    useEffect(() => {
+        if (time == 0) {
+            setTyping('')
+            setStartCountdown(false)
+            setTimeUp(true)
+            setTime(15)
+        }
+        if (startCountdown && time > 0) {
+            const timer = setInterval(() => {
+                setTime((prevTime) => prevTime - 1);
+            }, 1000);
+
+            return () => {
+                clearInterval(timer);
+            };
+        }
+    }, [startCountdown, time]);
+
+    return (
+        <div className='w-full flex items-center justify-between bg-neutral-content rounded-md p-2 shadow-md'>
+            <span className="countdown font-mono text-4xl">
+                <span style={{ "--value": time }}></span>
+            </span>
+            <h1>{typing}</h1>
+            <div className="tabs font-semibold">
+                <button value={15} className={btnActive == 15 ? "tab tab-active" : "tab"} onClick={onBtnClick}>15s</button>
+                <button value={30} className={btnActive == 30 ? "tab tab-active" : "tab"} onClick={onBtnClick}>30s</button>
+                <button value={45} className={btnActive == 45 ? "tab tab-active" : "tab"} onClick={onBtnClick}>45s</button>
+                <button value={60} className={btnActive == 60 ? "tab tab-active" : "tab"} onClick={onBtnClick}>60s</button>
+            </div>
+        </div>
+    )
+}
+
+export default TimeSelector
