edbda3f5b63a62f81b8369641837db222fc7a059
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-10-05 10:42:46 +0700

    add timeselector and sentence components

--- src/components/TypeBox.tsx
+++ src/components/TypeBox.tsx
@@ -1,38 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Typing from './Sentence'
+import Sentence from './Sentence'
+import TimeSelector from './TimeSelector'
 
-function TypeBox({ typing }: { typing: string }) {
-    const sentence = "run jump swim fly drive sail i you have has"
-    const chars = sentence.split('')
-    const words = sentence.split(' ')
-
-    const cssForChar = (index: number) => {
-        let css = ""
-        if (index == typing.length) {
-            css += 'char-active '
-        }
-        if (index >= typing.length) {
-            css += 'char-nontyped'
-        }
-        return css
-    }
+function TypeBox({ typing, setTyping }: { typing: string, setTyping: React.Dispatch<React.SetStateAction<string>> }) {
+    const sentence = "run jump swim fly drive sail i you have has apple banana orange mango watermelon grapefruit pineapple strawberry cherry lemon lime pear peach plum apricot coconut fig grape guava kiwi nectarine papaya pomegranate raspberry tangerine tomato avocado blackberry blackcurrant blueberry boysenberry currant gooseberry lychee mandarin melon cantaloupe honeydew nut peach nectarine persimmon plantain plum prune pineapple pomegranate quince raisin raspberry rambutan redcurrant salal salak satsuma star fruit tamarillo tamarind ugli fruit watermelon"
+    const [timeUp, setTimeUp] = React.useState(false)
 
     return (
-        <div className='h-full flex flex-col p-40 items-center gap-40'>
-            <div>
-                <h1>TYPING: {typing}</h1>
-                <h1>COUNT : {typing.length}</h1>
-            </div>
-            <div className='flex flex-wrap flex-row text-2xl font-semibold text-neutral tracking-widest ease-in'>
-                {chars.map((char, index) => {
-                    if (char == " ") {
-                        return <span key={index} className={cssForChar(index)}>&nbsp;</span>
-                    }
-                    return <span key={index} className={cssForChar(index)}>{char}</span>
-                })}
-            </div>
+        <div className='h-full flex flex-col p-40 items-center gap-40 w-10/12'>
+            <TimeSelector typing={typing} setTimeUp={setTimeUp} setTyping={setTyping} />
+            {/* {
+                timeUp ? <Sentence typing={typing} sentence={sentence}/> : 
+            } */}
+            <Sentence typing={typing} sentence={sentence} />
         </div >
     )
 }
 
 export default TypeBox
-
