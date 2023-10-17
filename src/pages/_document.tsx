1d7ca16c1d84a1902664da0c119d0be4b0c52dd4
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-10-17 21:30:06 +0700

    change app name and add icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,17 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            <Head>
+                <title>Kitto Type</title>
+                {/* <link rel='icon' href='./kb1=2.png' sizes='any' /> */}
+                <link rel='icon' href='./kb.webp' sizes='any' />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
 }
